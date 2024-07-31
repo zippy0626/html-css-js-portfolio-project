ffdbb90b19b35f34093ee3a25270668f46a8bed8
Michael T. <153778336+zippy0626@users.noreply.github.com>
2024-07-31 13:39:32 -0400

    Added New sections and styling

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
-// toggleMenu function, menu and icon variables set to document (our document), and method queryselector to select elements on our website.
+// .querySelector() method selects an element from the document. This is set to a variable. 
+
+// The .classList attribute is the list of classes from the element
+
+// .toggle() method adds/appends the class "open", this can be toggled on and off.
+
+// We can then access the .element.open in .css to change behavior when this function is called using onclick="toggleMenu()"
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links")
     const icon = document.querySelector(".hamburger-icon")
