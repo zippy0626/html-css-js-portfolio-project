240d3057fafa8113ab6912c9fdaa71ea2547b783
Michael T. <153778336+zippy0626@users.noreply.github.com>
2024-08-05 11:11:50 -0400

    Finished website for now

--- scripts.js
+++ scripts.js
@@ -6,9 +6,50 @@
 
 // We can then access the .element.open in .css to change behavior when this function is called using onclick="toggleMenu()"
 
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links")
     const icon = document.querySelector(".hamburger-icon")
     menu.classList.toggle("open")
     icon.classList.toggle("open")
+};
+
+function toggleDark() {
+    const allElements = document.querySelectorAll('*'); // all elements in a list
+
+    allElements.forEach(element => {
+        element.classList.toggle('dark-mode');
+    }  
+    );
+};
+
+function checkEmail() {
+    const textArea = document.querySelector(".email-textarea")
+    const emailField = document.querySelector(".email-field")
+    const noEmail = document.querySelector('.no-email-message')
+    const noText = document.querySelector(".error-message")
+    const confirmMsg = document.querySelector(".confirm-message")
+    
+
+    if (textArea.value.trim() === '' && emailField.value.trim() === '') {
+        noText.style.display = "flex";
+        noEmail.style.display = 'flex';
+        confirmMsg.style.display = 'none';
+    } else if (textArea.value.trim() === '') {
+        noText.style.display = "flex";
+        noEmail.style.display = 'none';
+        confirmMsg.style.display = 'none';
+    } else if (emailField.value.trim() === '') {
+        noText.style.display = "none";
+        noEmail.style.display = 'flex';
+        confirmMsg.style.display = 'none';
+    } else {
+        noText.style.display = "none";
+        noEmail.style.display = 'none';
+        confirmMsg.style.display = 'flex';
+    };
+
 }
